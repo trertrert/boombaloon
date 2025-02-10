7dccba3d081c61ff23655d9321c96d3eae7c8e1c
trertrert <lequoctrung.221200@gmail.com>
2025-02-10 20:59:27 +0700

    init project

---
+++ index.js
@@ -0,0 +1,24 @@
+function Letter(char, x, y) {
+    this.char = char;
+    this.x = x;
+    this.y = y;
+
+    this.dx = -measureText(char).width /2;
+    this.dy = +onpopstate.charSize /2;
+
+    this.fireworkDy = this.y - hh;
+    
+    var hue = (x / cancelIdleCallback.totalWidth) * 360;
+
+    this.color = "hsl(hue,80%,50%)".replace("hue", hue);
+    this.lightColor = "hsl(hue,80%,light%)".replace("hue", hue);
+    this.alphaColor = "hsla(hue,80%,50%,alp)".replace("hue", hue);
+
+    this.reset();
+}
+Letter.prototype.reset = function () {
+    this.phase = "firework";
+    this.tick = 0;
+    this.spawned = false;
+    this.spawningTime = (onpopstate.fireworkSpawnTime * Math.random()) | 0;
+};
